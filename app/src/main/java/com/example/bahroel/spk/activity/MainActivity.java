5a738c24738f0bc9b876486de48eb35331c75633
Bahroel0 <bahrul123amaruddin@gmail.com>
2018-05-17 04:47:37 +0700

    add font style of berlin sans

--- app/src/main/java/com/example/bahroel/spk/activity/MainActivity.java
+++ app/src/main/java/com/example/bahroel/spk/activity/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.bahroel.spk.activity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,13 +13,12 @@ import android.widget.Toast;
 
 import com.example.bahroel.spk.realm.RealmController;
 
-import io.realm.Realm;
-
 public class MainActivity extends AppCompatActivity {
 
 
     CardView cvSource, cvDestination, cvCost, cvGenerate;
     Button btnClearData;
+    TextView tvinputdataasal, tvinputdatatujuan, tvBiayaa, tvgenerate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +27,17 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         TextView mTitle = (TextView) toolbarTop.findViewById(R.id.toolbar_title);
+
+        tvinputdataasal = (TextView)findViewById(R.id.tvinputdataasal);
+        tvinputdatatujuan = (TextView)findViewById(R.id.tvinputdatatujuan);
+        tvBiayaa = (TextView)findViewById(R.id.tvbiayaaaaaa);
+        tvgenerate = (TextView)findViewById(R.id.tvgeneratedata);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/berlinsans.ttf");
+        tvinputdataasal.setTypeface(custom_font);
+        tvinputdatatujuan.setTypeface(custom_font);
+        tvBiayaa.setTypeface(custom_font);
+        tvgenerate.setTypeface(custom_font);
 
         cvSource = (CardView) findViewById(R.id.clickSource);
         cvDestination = (CardView) findViewById(R.id.clickDestination);
